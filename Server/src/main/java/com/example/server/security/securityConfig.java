9278d6596f5cfe6507f420dd8bcd670e1962d1a7
lorenzopaci01 <104992009+lorenzopaci01@users.noreply.github.com>
2022-05-11 19:48:47 +0200

    Update securityConfig.java

--- Server/src/main/java/com/example/server/security/securityConfig.java
+++ Server/src/main/java/com/example/server/security/securityConfig.java
@@ -1,6 +1,6 @@
 package com.example.server.security;
 
-import com.example.server.filter.CustomAuthorizationFilter;
+bambozzi fa piu serate che cene
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
